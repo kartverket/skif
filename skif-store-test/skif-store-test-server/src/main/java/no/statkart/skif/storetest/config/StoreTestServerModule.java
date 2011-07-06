2adb151aed96a8e19173798fde981d00c03c8fba
Henrik Fredholm <frehen@statkart.no>
2011-07-06 15:35:26 +0100

    Lagt til configurasjon og access av store i singlevm mode for StoreTest1Service
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 35818]

--- skif-store-test/skif-store-test-server/src/main/java/no/statkart/skif/storetest/config/StoreTestServerModule.java
+++ skif-store-test/skif-store-test-server/src/main/java/no/statkart/skif/storetest/config/StoreTestServerModule.java
@@ -2,10 +2,13 @@ package no.statkart.skif.storetest.config;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.google.inject.Singleton;
+import no.statkart.skif.ConfigurationConverter;
 import no.statkart.skif.ServiceMode;
 import no.statkart.skif.SkifModule;
 import no.statkart.skif.SkifUtil;
 import no.statkart.skif.config.Configuration;
+import no.statkart.skif.config.PropertiesConfiguration;
 import no.statkart.skif.module.ModuleStrategyFactory;
 import no.statkart.skif.service.ServiceRequestContext;
 import no.statkart.skif.service.chain.EJBServiceChainFactory;
@@ -15,9 +18,15 @@ import no.statkart.skif.service.module.server.ServerModule;
 import no.statkart.skif.service.proxy.ChainedProxyHandler;
 import no.statkart.skif.service.proxy.ProxyHandler;
 import no.statkart.skif.service.proxy.RuntimeExceptionProxyHandler;
+import no.statkart.skif.service.scope.ServiceRequestScoped;
+import no.statkart.skif.store.ReplicaVersion;
+import no.statkart.skif.store.persistence.hibernate.*;
+import no.statkart.skif.storetest.domain.TestBubble;
+import org.hibernate.Session;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.Method;
+import java.util.Properties;
 
 /**
  * @author Henrik Fredholm
@@ -37,6 +46,31 @@ public class StoreTestServerModule extends SkifModule {
     protected void configure() {
         install(new ServerModule(moduleConfiguration));
         install(new ServerServiceModule(moduleConfiguration, new StoreTestGroup1Services().getServices()));
+
+        // Quick hack for å definere opp Store. Dette skal vere en egen modul:
+        bindStore();
+    }
+
+
+
+    private StoreHibernateSessionFactoryBuilder createHibernateSessionFactoryBuilder() {
+        Configuration cfg = new PropertiesConfiguration(getClass().getResource("/no/statkart/skif/storetest/config/persistence/skiftest-hibernate-singlevm.properties"));
+        Properties properties = ConfigurationConverter.getProperties(cfg);
+        return new StoreHibernateSessionFactoryBuilder(properties, "no/statkart/skif/storetest/persistence/hibernate");
+    }
+
+    private void bindStore() {
+        StoreHibernateSessionFactoryBuilder sfbuilder = createHibernateSessionFactoryBuilder();
+        sfbuilder.addResource(TestBubble.class);
+
+        // Alle requester skal dele samme factory manager, mens session managers kun skal deles per request
+        bind(StoreHibernateSessionFactoryBuilder.class).toInstance(sfbuilder);
+        bind(StoreHibernateSessionFactoryManager.class).in(Singleton.class);
+        bind(StoreHibernateSessionManager.class).in(ServiceRequestScoped.class);
+        bind(ReplicaVersion.class).toInstance(ReplicaVersion.CURRENT);
+        bind(StoreHibernateSession.class).toProvider(StoreHibernateSessionProvider.class).in(ServiceRequestScoped.class);
+        bind(Session.class).toProvider(HibernateSessionProvider.class).in(ServiceRequestScoped.class);
+
     }
 }
 
--- skif-store-test/skif-store-test-server/src/main/java/no/statkart/skif/storetest/service/storetest1/StoreTest1ServiceImpl.java
+++ skif-store-test/skif-store-test-server/src/main/java/no/statkart/skif/storetest/service/storetest1/StoreTest1ServiceImpl.java
@@ -1,5 +1,11 @@
 package no.statkart.skif.storetest.service.storetest1;
 
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import no.statkart.skif.storetest.domain.TestBubble;
+import no.statkart.skif.storetest.domain.TestBubbleId;
+import org.hibernate.Session;
+
 import java.util.HashMap;
 
 /**
@@ -7,7 +13,10 @@ import java.util.HashMap;
  * @since 2.0
  */
 public class StoreTest1ServiceImpl implements StoreTest1Service {
-    static HashMap<String,String> map = new HashMap<String,String>();
+    static HashMap<String, String> map = new HashMap<String, String>();
+
+    @Inject
+    Provider<Session> sessionProvider;
 
     @Override
     public String put(String key, String value) {
@@ -16,6 +25,12 @@ public class StoreTest1ServiceImpl implements StoreTest1Service {
 
     @Override
     public String get(String key) {
+        Session s = sessionProvider.get();
+        TestBubble e = (TestBubble) s.get(TestBubble.class, new TestBubbleId(1));
+        if (e == null) {
+            throw new RuntimeException("Skal ikke kommer her");
+        }
+
         return map.get(key);
     }
 
