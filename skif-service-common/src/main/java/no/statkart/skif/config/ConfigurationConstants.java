bbc7ff37499074e886668613d3ea53a251aa5df3
Henrik Fredholm <frehen@statkart.no>
2011-08-11 12:11:18 +0100

    Oppdatert manglende fil
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 36055]

--- skif-service-common/src/main/java/no/statkart/skif/config/ConfigurationConstants.java
+++ skif-service-common/src/main/java/no/statkart/skif/config/ConfigurationConstants.java
@@ -7,9 +7,10 @@ package no.statkart.skif.config;
 public class ConfigurationConstants {
     private ConfigurationConstants() {}
 
-    public final static String SINGLE_VM_SERVER_INJECTOR = "skif.configuration.single_vm_serverInjector";
+    public final static String SINGLE_VM_SERVER_INJECTOR = "skif.single_vm_serverInjector";
     public final static String SINGLE_VM = "skif.single_vm";
     public static final String MODULE_CLASS = "skif.module_class";
+    public static final String USE_SHARED_SERVER = "skif.shared_server";
     public static final String CONFIGURATION_PROPERTIES = "skif.configuration_properties";
     public static final String CONFIGURATION_FILENAME = "skif.configuration_filename";
     public static final String MODULE_STRATEGY_FACTORY_CLASS = "skif.module_strategy_factory_class";
@@ -18,4 +19,15 @@ public class ConfigurationConstants {
     public static final String SINGLE_VM_SERVER_MODULE_STRATEGY_FACTORY_CLASS = "skif.singlevm_server_module_strategy_factory_class";
     public static final String SINGLEVM_SERVER_CONFIGURATION = "skif.single_vm_server_configuration_properties";
     public static final String SINGLE_VM_SERVER_CONFIGURATION_FILENAME = "skif.single_vm_server_configuration_filename";
+
+
+    public static final String SERVER_USERNAME = "skif.server_username";
+    public static final String SERVER_PASSWORD = "skif.server_password";
+    public static final String SERVER_URL = "skif.server_url";
+
+    public static final String DB_USERNAME = "skif.db_username";
+    public static final String DB_PASSWORD = "skif.db_password";
+    public static final String DB_HOSTNAME = "skif.db_hostname";
+    public static final String DB_PORT = "skif.db_port";
+    public static final String DB_SID = "skif.db_sid";
 }
