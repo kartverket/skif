85e7d45db44a032886554c4d62f4613294588b5c
Leif Eirik Lislegård <lislei@statkart.no>
2011-08-24 13:09:08 +0100

    MATH-32: common-testsupport publiseres nå. Faktorert ut versjonsnr for bibliotek (avhengigheter).
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 36265]

--- setEnv_personal - template.cmd
+++ setEnv_personal - template.cmd
@@ -13,7 +13,7 @@ set GRADLE_HOME=c:\apps\gradle-1.0-milestone-4
 set WEBLOGIC_HOME=C:\Oracle\Middleware
 
 @rem Definer lokal nexus repo for offline utvikling (optional)
-@rem set PUBLIC_REPO=http://localhost:8081/nexus/content/repositories/statkart/
+@rem set REPO_PUBLIC=http://localhost:8081/nexus/content/repositories/statkart/
 
 @rem Setup Perforce windows integrasjon (optional)
 @rem p4 set P4CLIENT=frehen-WSFREHEN2
