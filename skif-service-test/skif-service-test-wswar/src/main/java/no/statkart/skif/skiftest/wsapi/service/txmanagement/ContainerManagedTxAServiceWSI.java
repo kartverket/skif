beeb4b7e132b7614d34865ddf1114cebd63bca78
Henrik Fredholm <frehen@statkart.no>
2011-08-31 10:44:44 +0100

    Lagt til Bean og Container Managed Tranasction håndterin
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 36354]

---
+++ skif-service-test/skif-service-test-wswar/src/main/java/no/statkart/skif/skiftest/wsapi/service/txmanagement/ContainerManagedTxAServiceWSI.java
@@ -0,0 +1,17 @@
+package no.statkart.skif.skiftest.wsapi.service.txmanagement;
+
+import no.statkart.skif.service.ws.ServiceWSI;
+import no.statkart.skif.skiftest.wsapi.exception.ServiceException;
+
+import javax.jws.WebParam;
+
+/**
+ * @author Henrik Fredholm
+ * @since 1.1
+ */
+public interface ContainerManagedTxAServiceWSI extends ServiceWSI {
+    public void clear() throws ServiceException;
+    public String get(@WebParam(name = "key") String key) throws ServiceException;
+    public String put(@WebParam(name = "key") String key, @WebParam(name = "value") String value) throws ServiceException;
+    public void multiPut(@WebParam(name = "key1") String key1, @WebParam(name = "value1") String value1, @WebParam(name = "key2") String key2, @WebParam(name = "value2") String value2) throws ServiceException;
+}
