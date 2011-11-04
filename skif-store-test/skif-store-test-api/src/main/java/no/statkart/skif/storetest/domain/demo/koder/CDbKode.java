fe8e91c3a6ff5b5fc2bd1108a4db3bd924ec693a
Henrik Fredholm <frehen@statkart.no>
2011-11-04 09:29:33 +0100

    SKIF-96: Har fjernet StoreTestDbSubclassedKode*. Bruker nå CDbKode* direkte istedet
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 37643]

--- skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/demo/koder/CDbKode.java
+++ skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/demo/koder/CDbKode.java
@@ -1,15 +1,14 @@
 package no.statkart.skif.storetest.domain.demo.koder;
 
+import no.statkart.skif.store.kodelistesupport.DbKode;
 import no.statkart.skif.storetest.domain.kode.StoreTestDbKode;
-import no.statkart.skif.storetest.domain.kode.StoreTestDbSubclassedKodeImpl;
-import no.statkart.skif.storetest.domain.kode.StoreTestDbSubclassedKodeImplId;
 
 
 /**
  * @author Henrik Fredholm
  * @since 0.6
  */
-public abstract class CDbKode extends StoreTestDbSubclassedKodeImpl implements StoreTestDbKode {
+public abstract class CDbKode extends DbKode implements StoreTestDbKode {
     @Override
     public CDbKodeId<?> getId() {
         return (CDbKodeId<?>)super.getId();    //To change body of overridden methods use File | Settings | File Templates.
--- skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/demo/koder/CDbKodeId.java
+++ skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/demo/koder/CDbKodeId.java
@@ -1,23 +1,63 @@
 package no.statkart.skif.storetest.domain.demo.koder;
 
 import no.statkart.skif.store.SnapshotVersion;
+import no.statkart.skif.store.kodelistesupport.DbKodeId;
+import no.statkart.skif.store.kodelistesupport.DbKodeSupport;
 import no.statkart.skif.storetest.domain.kode.StoreTestDbKodeId;
-import no.statkart.skif.storetest.domain.kode.StoreTestDbSubclassedKodeImplId;
 
 /**
  * Id superklasse for alle koder av type CDbKode. Denne klasse kan ikke være abstrakt siden hibernate oppretter
  * instanser av denne type når idene lastes. I hibernate interceptoren bytte CDbKodeId ut med id av riktig
  * subtype.
  *
- * Det at en kode aver fra CDbKode betyder i denne sammenheng blot at koden skal hentes fra tabellen til CDbKode.
+ * Det at en kode arver fra CDbKode betyder i denne sammenheng blot at koden skal hentes fra tabellen til CDbKode.
+ * Det vil f.eks ikke være noen kodeliste som inneholder alle kode av type CDbKode. Det er kun kodelister for
+ * hver individuel kode. Det vil heller ikke være noen domeneobjekter som har felter av type CDbKode. Det har ingen
+ * mening.
  *
  * @author Henrik Fredholm
  * @since 2.0
  */
-public class CDbKodeId<T extends CDbKode> extends StoreTestDbSubclassedKodeImplId<T> implements StoreTestDbKodeId<T> {
-
+public class CDbKodeId<T extends CDbKode> extends DbKodeId<T> implements StoreTestDbKodeId<T> {
     protected CDbKodeId(Long value, SnapshotVersion snapshotVersion) {
         super(value, snapshotVersion);
+    }
+
+    @Override
+    public boolean equals(Object id) {
+        if (this==id) return true;
+        return (id!=null && id instanceof CDbKodeId && ((CDbKodeId) id).getValue().equals((((CDbKodeId) id).getValue())) && this.getSnapshotVersion()==((CDbKodeId) id).getSnapshotVersion());
+    }
+
+    /**
+     * For endelige subklasser av DbSubclassedKodeId returneres unike instanser per kode verdi. For mellomliggende
+     * subklasser returners det ikke unike instanser per kode verdi. Se {@link {#getKodeSupport}} for videre forklaring.
+     *
+     * @return
+     */
+    @Override
+    public final CDbKodeId<T> resolveInstance() {
+        DbKodeSupport kodeSupport = getKodeSupport();
+        if (kodeSupport==null) {
+            return this;
+        } else {
+            return (CDbKodeId<T>) getKodeSupport().getOrCreateInstance(this);
+        }
+    }
+
+
+    /**
+     * Endelige subklasser av DbSubclassedKodeId vil (per design) alltid overskrive denne og dermed ha unike instanser
+     * per id value. Mellomliggende subklasser av DbSubclassedKodeId har ingen kodesupport og deres instanser blir
+     * dermed ikke unike per kode verdi. Mellomliggende id subklasser brukes kun internt i Skif rammeverket ifm lasting av
+     * Kode klassen og byttes alltid ut med endlig id subklasse før klassen er ferdig lastet. Dette skjer i
+     * no.statkart.skif.persistence.hibernate.HibernateInterceptor#onLoad
+     *
+     * @return
+     */
+    @Override
+    protected DbKodeSupport getKodeSupport() {
+        return null;
     }
 
 }
