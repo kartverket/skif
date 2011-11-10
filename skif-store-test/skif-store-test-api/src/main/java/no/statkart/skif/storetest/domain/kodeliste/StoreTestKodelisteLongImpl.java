b5782f54e2e959bf84e8f70bb1f6a028d5f95424
Steinar Hansen <hanste@statkart.no>
2011-11-10 13:24:01 +0100

    Integration change.
    [git-p4: depot-paths = "//skif/SkifKode/devStore/": change = 37739]

--- skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/kodeliste/StoreTestKodelisteLongImpl.java
+++ skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/kodeliste/StoreTestKodelisteLongImpl.java
@@ -1,6 +1,6 @@
 package no.statkart.skif.storetest.domain.kodeliste;
 
-import no.statkart.skif.store.kodelistesupport.Kodeliste;
+import no.statkart.skif.store.kodeliste.Kodeliste;
 
 /**
  * @author Henrik Fredholm
