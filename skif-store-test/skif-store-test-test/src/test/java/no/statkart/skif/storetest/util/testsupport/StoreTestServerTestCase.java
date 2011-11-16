ea265d1d0b552eb3c7ec488fa4c28e9a0b18a145
Henrik Fredholm <frehen@statkart.no>
2011-11-16 15:13:25 +0100

    Har opprettet en StoreTestServerTestCase
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 37941]

---
+++ skif-store-test/skif-store-test-test/src/test/java/no/statkart/skif/storetest/util/testsupport/StoreTestServerTestCase.java
@@ -0,0 +1,16 @@
+package no.statkart.skif.storetest.util.testsupport;
+
+import no.statkart.skif.storetest.config.StoreTestClientModule;
+import no.statkart.skif.storetest.config.StoreTestServerModule;
+import no.statkart.skif.util.testsupport.SkifServerTestCase;
+import no.statkart.skif.util.testsupport.SkifTestCase;
+
+/**
+ * @author Henrik Fredholm
+ */
+public class StoreTestServerTestCase extends SkifServerTestCase {
+    public StoreTestServerTestCase() {
+        setModuleClass(StoreTestClientModule.class);
+        setSingleVmServerModuleClass(StoreTestServerModule.class);
+    }
+}
