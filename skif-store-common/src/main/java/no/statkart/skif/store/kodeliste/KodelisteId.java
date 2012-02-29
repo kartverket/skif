a82623494cca18ccc44fe0f3d9499de5ead27a7c
Henrik Fredholm <frehen@statkart.no>
2012-02-29 10:00:54 +0100

    Lagt til manglende fil
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 39793]

---
+++ skif-store-common/src/main/java/no/statkart/skif/store/kodeliste/KodelisteId.java
@@ -0,0 +1,24 @@
+package no.statkart.skif.store.kodeliste;
+
+import no.statkart.skif.store.BubbleId;
+import no.statkart.skif.store.SnapshotVersion;
+
+/**
+ * Interface for Id klassen hørende til {@link Kodeliste}.
+ *
+ * @author Henrik Fredholm
+ * @since 2.1
+ */
+public interface KodelisteId<T extends Kodeliste> extends BubbleId<T> {
+    @Override
+    KodelisteId<T> asSnapshotVersion(BubbleId<?> bubbleId);
+
+    @Override
+    KodelisteId<T> asSnapshotVersion(SnapshotVersion snapshotVersion);
+
+    @Override
+    KodelisteId<T> asSnapshotVersionCurrent();
+
+    @Override
+    KodelisteId<T> asSnapshotVersionOld();
+}
