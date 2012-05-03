7a72246d9941b9aecff6d2edffdf6965c893c018
kvaodd <a@b>
2012-05-03 10:33:35 +0100

    Lagt til hjelpemetoder for å hente faktiske andelar, ikkje berre andelsids.
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 40856]

--- skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/tinglysing/HjemmelForMatrikkelenhet.java
+++ skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/tinglysing/HjemmelForMatrikkelenhet.java
@@ -17,20 +17,29 @@ public class HjemmelForMatrikkelenhet extends Hjemmel {
         return (HjemmelForMatrikkelenhetId<?>) super.getId();
     }
 
-
     public Set<AndelIMatrikkelenhetId<?>> getNyeAndelIds() {
         return nyeAndelIds;
     }
 
+    public Set<AndelIMatrikkelenhet> getNyeAndeler() {
+        return store().get(nyeAndelIds);
+    }
+
     public void setNyeAndelIds(Set<AndelIMatrikkelenhetId<?>> nyeAndelIds) {
-        this.nyeAndelIds = nyeAndelIds;
+        this.nyeAndelIds.clear();
+        this.nyeAndelIds.addAll(nyeAndelIds);
     }
 
     public Set<AndelIMatrikkelenhetId<?>> getUtgaatteAndelIds() {
         return utgaatteAndelIds;
     }
 
+    public Set<AndelIMatrikkelenhet> getUtgaatteAndeler() {
+        return store().get(utgaatteAndelIds);
+    }
+    
     public void setUtgaatteAndelIds(Set<AndelIMatrikkelenhetId<?>> utgaatteAndelIds) {
-        this.utgaatteAndelIds = utgaatteAndelIds;
+        this.utgaatteAndelIds.clear();
+        this.utgaatteAndelIds.addAll(utgaatteAndelIds);
     }
 }
