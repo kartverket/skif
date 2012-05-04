be292ed42cf9a3d85e328196105bc0a860230193
Christian Rørdam <rorchr@statkart.no>
2012-05-04 10:01:38 +0100

    Ny modell for Tinglysing: gjorde konstanter final
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 40893]

--- skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/tinglysing/RettsstiftelsestypeKodeId.java
+++ skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/tinglysing/RettsstiftelsestypeKodeId.java
@@ -9,13 +9,14 @@ import no.statkart.skif.storetest.domain.kodeliste.StoreTestKodelisteLongId;
  * @author rorchr
  */
 public class RettsstiftelsestypeKodeId extends StoreTestDbKodeId<RettsstiftelsestypeKode> {
-    private static StoreTestDbKodeSupport<RettsstiftelsestypeKodeId> kodeSupport = new StoreTestDbKodeSupport<RettsstiftelsestypeKodeId>(RettsstiftelsestypeKodeId.class, 1L);
 
-    public static StoreTestKodelisteLongId<?> KODELISTE_ID = kodeSupport.getKodelisteId();
-    public static RettsstiftelsestypeKodeId HJ_HJG = define(1); // TODO: finn id etter at loadTestdata.sql er laget
-    public static RettsstiftelsestypeKodeId FE_FES = define(2); // TODO: finn id etter at loadTestdata.sql er laget
+    private static final StoreTestDbKodeSupport<RettsstiftelsestypeKodeId> kodeSupport = new StoreTestDbKodeSupport<RettsstiftelsestypeKodeId>(RettsstiftelsestypeKodeId.class, 1L);
+    public static final StoreTestKodelisteLongId<?> KODELISTE_ID = kodeSupport.getKodelisteId();
+    
+    public static final RettsstiftelsestypeKodeId HJ_HJG = define(1); // TODO: finn id etter at loadTestdata.sql er laget
+    public static final RettsstiftelsestypeKodeId FE_FES = define(2); // TODO: finn id etter at loadTestdata.sql er laget
     public static final RettsstiftelsestypeKodeId JS_JSA = define(3);  // TODO: finn id etter at loadTestdata.sql er laget
-    public static RettsstiftelsestypeKodeId PA_PAF = define(100); // TODO: finn id etter at loadTestdata.sql er laget
+    public static final RettsstiftelsestypeKodeId PA_PAF = define(100); // TODO: finn id etter at loadTestdata.sql er laget
 
     public RettsstiftelsestypeKodeId(Long value, SnapshotVersion snapshotVersion) {
         super(value, snapshotVersion);
