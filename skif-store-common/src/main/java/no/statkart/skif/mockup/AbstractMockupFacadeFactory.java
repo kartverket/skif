c567c4ab5a5312f2c441bc9dcf3c6f257506707b
Roar Ingebrigtsen <IngRoa@statkart.no>
2012-06-28 10:15:08 +0100

    MAT-10063: Har lagt inn hard referanse til testNumber_0 i mockup slik at man lett har tilgang til dette
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 41870]

--- skif-store-common/src/main/java/no/statkart/skif/mockup/AbstractMockupFacadeFactory.java
+++ skif-store-common/src/main/java/no/statkart/skif/mockup/AbstractMockupFacadeFactory.java
@@ -1,6 +1,7 @@
 package no.statkart.skif.mockup;
 
 import com.google.inject.*;
+import com.google.inject.name.Names;
 import no.statkart.skif.service.sequence.IdService;
 import no.statkart.skif.service.test.TestdataService;
 import no.statkart.skif.store.*;
@@ -95,10 +96,14 @@ public abstract class AbstractMockupFacadeFactory<T extends AbstractMockupFacade
 
 
     private T createFacade(final TestNumber testNumber) {
+
+        final TestNumber testNumber_0 = testdataService.getTestNumber_0();
+
         Module module = new AbstractModule() {
             @Override
             protected void configure() {
                 bind(TestNumber.class).toInstance(testNumber);
+                bind(TestNumber.class).annotatedWith(Names.named("testNumber_0")).toInstance(testNumber_0);
                 bind(IdService.class).to(idServiceImplementationClass);
                 bind(Store.class).to(MockupStore.class);
             }
