d1ec21dafd35f4818a62e591cccd967350383758
Roar Ingebrigtsen <IngRoa@statkart.no>
2012-07-17 08:04:13 +0100

    Lagt til insert="false" og update="false" for å teste dette for tidspunktsinfo. Har også lagt til et par tester på BubbleWithList
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 42097]

--- skif-store-test/skif-store-test-test/src/test/java/no/statkart/skif/storetest/service/store/StoreUpdateComponentTest.java
+++ skif-store-test/skif-store-test-test/src/test/java/no/statkart/skif/storetest/service/store/StoreUpdateComponentTest.java
@@ -13,11 +13,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Calendar;
+import java.util.HashSet;
 
 /**
  * Test som tester at vi får lagt til en entitycomponent i en liste på et bobleobjekt uten at eksisterende elementer
  * i listen får nye versjoner.
- *
+ * <p/>
  * Se SKIF-210 for mer informasjon om problemet som testes her.
  *
  * @author Roar Ingebrigtsen
@@ -61,7 +62,7 @@ public class StoreUpdateComponentTest extends StoreTestServerTestCase {
             Assert.assertEquals(bubbleWithListComponent.gettVersion(), 1);
         }
 
-         server.runInTxRequiresNew(new RunOnServerMethod() {
+        server.runInTxRequiresNew(new RunOnServerMethod() {
             @Inject
             Store store;
 
@@ -88,5 +89,68 @@ public class StoreUpdateComponentTest extends StoreTestServerTestCase {
             }
         });
 
+    }
+
+    /**
+     *
+     */
+    public void testMapUtOgTilbake() {
+
+        server.runInTxRequiresNew(new RunOnServerMethod() {
+            @Inject
+            Store store;
+
+            @Override
+            public Object run() {
+                BubbleWithListId<BubbleWithList> bubbleId = new BubbleWithListId<BubbleWithList>(2201L);
+                BubbleWithList bubbleWithList = store.get(bubbleId);
+                store.evict(bubbleId);
+
+                BubbleWithList copy = new BubbleWithList(bubbleWithList);
+                store.lock(copy.getId());
+
+                store.update(copy);
+                return null;
+            }
+        });
+
+        BubbleWithList bubbleWithList = store.get(new BubbleWithListId<BubbleWithList>(2201L));
+        for (BubbleWithListComponent bubbleWithListComponent : bubbleWithList.getComponents()) {
+            Assert.assertEquals(bubbleWithListComponent.gettVersion(), 1);
+        }
+    }
+
+    public void testUpdateUtenAtObjektErLastetIHibernate(){
+
+        final long idToUse = Calendar.getInstance().getTimeInMillis();
+
+        server.runInTxRequiresNew(new RunOnServerMethod() {
+            @Inject
+            Store store;
+
+            @Override
+            public Object run() {
+                BubbleWithListId<BubbleWithList> bubbleId = new BubbleWithListId<BubbleWithList>(2201L);
+
+                BubbleWithList copy = new BubbleWithList();
+                copy.setId(bubbleId);
+                copy.setComponents(new HashSet<BubbleWithListComponent>());
+                BubbleWithListComponent e = new BubbleWithListComponent(2202L, "component for 2201", AEnumKodeId.KodeAId);
+                e.setBubbleWithList(copy);
+                copy.getComponents().add(e);
+                BubbleWithListComponent e1 = new BubbleWithListComponent(idToUse, "dette er component nr 2", AEnumKodeId.KodeBId);
+                e1.setBubbleWithList(copy);
+                copy.getComponents().add(e1);
+                store.lock(copy.getId());
+
+                store.update(copy);
+                return null;
+            }
+        });
+
+        BubbleWithList bubbleWithList = store.get(new BubbleWithListId<BubbleWithList>(2201L));
+        for (BubbleWithListComponent bubbleWithListComponent : bubbleWithList.getComponents()) {
+            Assert.assertEquals(bubbleWithListComponent.gettVersion(), 1);
+        }
     }
 }
