63c895eca96a2daee4d1adaa57888561be9682cc
Leif Eirik Lislegård <lislei@statkart.no>
2012-09-03 13:41:46 +0100

    grants for databasebruker
    - dette for opprettelse av indexer mm
    
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 42596]

--- database/src/main/sql/oracle/schema/GrantRole.sql
+++ database/src/main/sql/oracle/schema/GrantRole.sql
@@ -1,3 +1,10 @@
 GRANT CREATE TYPE TO FREHEN;
 GRANT CREATE PROCEDURE TO FREHEN;
 GRANT CREATE ANY TRIGGER TO FREHEN;
+
+-- trenger noen privilegier for opprettelse og patching av schema
+-- todo: vurdere om en ønsker å revoke disse i byggesystemet etter at schema er opprettet og patchet opp.
+GRANT CREATE SEQUENCE TO "FREHEN";
+GRANT CREATE SESSION TO "FREHEN";
+GRANT CREATE TABLE TO "FREHEN";
+GRANT CREATE VIEW TO "FREHEN";
