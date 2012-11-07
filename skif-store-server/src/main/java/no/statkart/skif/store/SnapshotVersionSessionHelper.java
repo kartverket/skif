e79df4dcc6c6e5c1dd6a3403b0099cdb476ebda8
Henrik Fredholm <frehen@statkart.no>
2012-11-07 12:17:34 +0100

    Fixet problem med bruk av Hibernate.TIMESTAMP som er deprecated i 3.6.10
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 43833]

--- skif-store-server/src/main/java/no/statkart/skif/store/SnapshotVersionSessionHelper.java
+++ skif-store-server/src/main/java/no/statkart/skif/store/SnapshotVersionSessionHelper.java
@@ -5,6 +5,7 @@ import org.hibernate.Hibernate;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.exception.SQLGrammarException;
+import org.hibernate.type.TimestampType;
 
 import java.sql.Timestamp;
 
@@ -15,6 +16,9 @@ import static no.statkart.skif.store.SnapshotVersionHelper.calcJustBeforeOf;
  * @since 2.1
  */
 public class SnapshotVersionSessionHelper {
+    // TODO: Virker ikke hvis Hibernate.TIMESTAMP brukes. Deprecated i 3.6.10
+    private static TimestampType TIMESTAMP = new TimestampType();
+
     public static void setSnapshotToJustBefore(Session session, SnapshotVersion snapshotVersion) {
         final Timestamp timestamp = calcJustBeforeOf(snapshotVersion.getTimestamp());
         session.createSQLQuery("select snapshot_time.set_t(:timestamp) from dual").setTimestamp("timestamp", timestamp).executeUpdate();
@@ -46,7 +50,8 @@ public class SnapshotVersionSessionHelper {
      */
     public static SnapshotVersion getSnapshotVersionNow(Session session) {
         final SQLQuery sqlQuery = session.createSQLQuery("select systimestamp as t from dual");
-        sqlQuery.addScalar("t", Hibernate.TIMESTAMP);
+        sqlQuery.addScalar("t", TIMESTAMP);
+
         return SnapshotVersion.createInstance((Timestamp) sqlQuery.uniqueResult());
     }
 
@@ -59,7 +64,7 @@ public class SnapshotVersionSessionHelper {
     public static SnapshotVersion getSnapshotVersion(Session session) {
         try {
             final SQLQuery sqlQuery = session.createSQLQuery("select snapshot_time.get_t() as t from dual");
-            sqlQuery.addScalar("t", Hibernate.TIMESTAMP);
+            sqlQuery.addScalar("t", TIMESTAMP);
             return SnapshotVersion.createInstance((Timestamp) sqlQuery.uniqueResult());
         } catch (SQLGrammarException e) {
             if (e.getErrorCode() == 904 && e.getSQLState().equals("42000") && e.getCause().getMessage().equals("ORA-00904: \"SNAPSHOT_TIME\".\"GET_T\": ugyldig identifikator\n")) {
