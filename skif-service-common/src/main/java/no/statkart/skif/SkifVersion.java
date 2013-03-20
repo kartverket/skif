99ad1946ea5da0de4914f57bd67232d602ef3f15
Leif Eirik Lislegård <lislei@statkart.no>
2013-03-20 15:10:54 +0100

    SKIF-266: Versjon informasjon for SKIF
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 46562]

---
+++ skif-service-common/src/main/java/no/statkart/skif/SkifVersion.java
@@ -0,0 +1,60 @@
+package no.statkart.skif;
+
+/**
+ * SKIF-266: Versjonsinformasjon for SKIF
+ *
+ * Det er viktig at byggesystemet legger inn manifest informasjon som behøves her.
+ *
+ * @author Leif Lislegård
+ * @since 2.2
+ */
+public class SkifVersion {
+
+    public static final SkifVersionImpl COMMON = forModule(SkifVersion.class);
+
+    public static SkifVersionImpl forModule(Class forModuleClass) {
+        return new SkifVersionImpl(forModuleClass);
+    }
+
+    public static String getTitle() {
+        return COMMON.getTitle();
+    }
+
+    public static String getVersion() {
+        return COMMON.getVersion();
+    }
+
+    public static boolean isProductionVersion() {
+        return COMMON.isProductionVersion();
+    }
+
+
+
+
+    static class SkifVersionImpl {
+        private final Class forModuleClass;
+
+        SkifVersionImpl(Class forModule) {
+            this.forModuleClass = forModule;
+        }
+
+        public String getTitle() {
+            return forModuleClass.getPackage().getImplementationTitle();
+        }
+
+        public String getVersion() {
+            return forModuleClass.getPackage().getImplementationVersion();
+        }
+
+        public boolean isProductionVersion() {
+            String implementationTitle = "" + forModuleClass.getPackage().getImplementationTitle();
+            return implementationTitle.contains("Produksjon");
+        }
+
+    }
+
+
+
+
+
+}
