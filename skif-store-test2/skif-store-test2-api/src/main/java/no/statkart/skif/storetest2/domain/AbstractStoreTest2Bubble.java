19c7cc88564897b3fed0aaacb9fa247dbb804caf
Tor Egil Strand <strant@statkart.no>
2013-03-22 10:45:49 +0100

    SKIF-269: Grunnklasser for storetest2
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 46604]

---
+++ skif-store-test2/skif-store-test2-api/src/main/java/no/statkart/skif/storetest2/domain/AbstractStoreTest2Bubble.java
@@ -0,0 +1,17 @@
+package no.statkart.skif.storetest2.domain;
+
+import no.statkart.skif.store.AbstractBubbleObject;
+import no.statkart.skif.storetest2.domain.StoreTest2Bubble;
+
+/**
+ * @author Roar Ingebrigtsen
+ * @since 2.2.0
+ */
+public class AbstractStoreTest2Bubble extends AbstractBubbleObject implements StoreTest2Bubble {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public AbstractStoreTest2BubbleId<?> getId() {
+        return (AbstractStoreTest2BubbleId<?>) super.getId();
+    }
+}
---
+++ skif-store-test2/skif-store-test2-api/src/main/java/no/statkart/skif/storetest2/domain/StoreTest2BubbleId.java
@@ -0,0 +1,11 @@
+package no.statkart.skif.storetest2.domain;
+
+import no.statkart.skif.store.BubbleId;
+
+/**
+ * Alle bobleid-er (inkludert kodeid-er) implementerer dette interfacet.
+ * @author Henrik Fredholm
+ * @since 2.0
+ */
+public interface StoreTest2BubbleId<T extends StoreTest2Bubble> extends BubbleId<T> {
+}
