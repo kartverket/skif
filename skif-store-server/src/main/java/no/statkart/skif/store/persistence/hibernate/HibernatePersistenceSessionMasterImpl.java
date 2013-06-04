8937694e79a65b7e0ea7502e84c9318049e82bd8
Tor Egil Strand <strant@statkart.no>
2013-06-04 13:57:47 +0100

    SKIF-296: commonLength ble ikke satt dersom den egentlig skulle vært lik antall egenskaper på den ene eller andre typen
    [git-p4: depot-paths = "//skif/SkifKode/2.1/": change = 48329]

--- skif-store-server/src/main/java/no/statkart/skif/store/persistence/hibernate/HibernatePersistenceSessionMasterImpl.java
+++ skif-store-server/src/main/java/no/statkart/skif/store/persistence/hibernate/HibernatePersistenceSessionMasterImpl.java
@@ -441,12 +441,11 @@ public class HibernatePersistenceSessionMasterImpl implements HibernatePersisten
         Type[] typesExisting = persisterExisting.getPropertyTypes();
         Object[] valuesExisting = persisterExisting.getPropertyValues(existingObject, EntityMode.POJO);
 
-        int commonLength = 0;
+        int commonLength;
         if (persister != persisterExisting) {
             // Beregn felles felter for object og existingObject
-            for (int i = 0; i < types.length && i < typesExisting.length; i++) {
-                if (!types[i].getName().equals(typesExisting[i].getName())) {
-                    commonLength = i;
+            for (commonLength = 0; commonLength < types.length && commonLength < typesExisting.length; commonLength++) {
+                if (!types[commonLength].getName().equals(typesExisting[commonLength].getName())) {
                     break;
                 }
             }
