c8a6c801c22e776f5f5ee20e11e9c19fdaed9392
Leif Eirik Lislegård <lislei@statkart.no>
2013-08-22 14:54:16 +0100

    GBOK-1935: Fnuttering
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 49682]

--- database/src/main/sql/oracle/schema/Purge.sql
+++ database/src/main/sql/oracle/schema/Purge.sql
@@ -2,7 +2,7 @@ PROMPT purging views for user @db_username@
 BEGIN
   FOR i IN (SELECT object_name FROM all_objects where owner = '@db_username@' AND object_type = 'VIEW')
     LOOP
-        EXECUTE IMMEDIATE('DROP VIEW @db_schema@.' || i.object_name);
+        EXECUTE IMMEDIATE('DROP VIEW "@db_schema@"."' || i.object_name || '"');
     END LOOP;
 END;
 /
@@ -13,7 +13,7 @@ PROMPT purging tables for user @db_username@
 BEGIN
   FOR i IN (SELECT table_name FROM all_tables where owner = '@db_username@' and table_name not like '%$%')
     LOOP
-        EXECUTE IMMEDIATE('DROP TABLE @db_schema@.' || i.table_name || ' CASCADE CONSTRAINTS PURGE');
+        EXECUTE IMMEDIATE('DROP TABLE "@db_schema@"."' || i.table_name || '" CASCADE CONSTRAINTS PURGE');
     END LOOP;
 END;
 /
@@ -22,7 +22,7 @@ PROMPT purging packages for user @db_username@
 BEGIN
   FOR i IN (SELECT object_name FROM all_objects where owner = '@db_username@' AND object_type = 'PACKAGE')
     LOOP
-        EXECUTE IMMEDIATE('DROP PACKAGE @db_schema@.' || i.object_name);
+        EXECUTE IMMEDIATE('DROP PACKAGE "@db_schema@"."' || i.object_name || '"');
     END LOOP;
 END;
 /
@@ -31,7 +31,7 @@ PROMPT purging functions for user @db_username@
 BEGIN
   FOR i IN (SELECT object_name FROM all_objects where owner = '@db_username@' AND object_type = 'FUNCTION')
     LOOP
-        EXECUTE IMMEDIATE('DROP FUNCTION @db_schema@.' || i.object_name);
+        EXECUTE IMMEDIATE('DROP FUNCTION "@db_schema@"."' || i.object_name || '"');
     END LOOP;
 END;
 /
@@ -40,7 +40,7 @@ PROMPT purging synonyms for user @db_username@
 BEGIN
   FOR i IN (SELECT object_name FROM all_objects where owner = '@db_username@' AND object_type = 'SYNONYM')
     LOOP
-        EXECUTE IMMEDIATE('DROP SYNONYM @db_schema@.' || i.object_name);
+        EXECUTE IMMEDIATE('DROP SYNONYM "@db_schema@"."' || i.object_name || '"');
     END LOOP;
 END;
 /
