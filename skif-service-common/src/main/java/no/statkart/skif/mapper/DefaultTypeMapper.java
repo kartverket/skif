3c0ab2be5f77fa2303b54bf13c618cc6e1be6791
Tor Egil Strand <strant@statkart.no>
2013-10-07 08:23:06 +0100

    SKIF-361: Bare forsøk å map rene gettere
    [git-p4: depot-paths = "//skif/SkifKode/2.3/": change = 50756]

--- skif-service-common/src/main/java/no/statkart/skif/mapper/DefaultTypeMapper.java
+++ skif-service-common/src/main/java/no/statkart/skif/mapper/DefaultTypeMapper.java
@@ -896,7 +896,7 @@ public class DefaultTypeMapper {
         List<Method> getters = new ArrayList<Method>(methods.length / 2);
         List<Method> idGetters = new ArrayList<Method>(methods.length / 4);
         for (Method method : methods) {
-            if (!method.isBridge() && (method.getName().startsWith("get") || method.getName().startsWith("is"))) {
+            if (!method.isBridge() && method.getParameterTypes().length == 0 && (method.getName().startsWith("get") || method.getName().startsWith("is"))) {
                 getters.add(method);
                 if (method.getName().endsWith("Id")) {
                     idGetters.add(method);
