60cae3cfed3cfb9194ee0e1fa3eaf2b13654c06e
Henrik Fredholm <frehen@statkart.no>
2013-10-14 21:41:05 +0100

    compilefix
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 51001]

--- skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/relation/uni/component/entity/X2EntityComponentMany.java
+++ skif-store-test/skif-store-test-api/src/main/java/no/statkart/skif/storetest/domain/relation/uni/component/entity/X2EntityComponentMany.java
@@ -7,7 +7,7 @@ import java.util.Set;
  * @since 2.4
  */
 public class X2EntityComponentMany {
-    private X2AAWithEntityComponent owner;
-    private X2EOne x2EOne;
-    private Set<X2FMany> x2FManySet;
+    //private X2AAWithEntityComponent owner;
+    //private X2EOne x2EOne;
+    //private Set<X2FMany> x2FManySet;
 }
