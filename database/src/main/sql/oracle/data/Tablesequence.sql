f1af21801a0f1689cf5b6ce28f3227bb7ef1de91
Henrik Fredholm <frehen@statkart.no>
2013-11-20 14:46:02 +0100

    Endret tablesequence.GLOBAL_SEQUENCE til å starte fra 99999999 slik at alle dynamisk allokerte genererte ids er større enn alle mockupids. Feil rekkefølge har gjort at endringslogg tester feiler
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 52154]

--- database/src/main/sql/oracle/data/Tablesequence.sql
+++ database/src/main/sql/oracle/data/Tablesequence.sql
@@ -1,3 +1,3 @@
-insert into TableSequence (tableName, nextFreeNumber) values ('GLOBAL_SEQUENCE',999999);
+insert into TableSequence (tableName, nextFreeNumber) values ('GLOBAL_SEQUENCE',99999999 );
 insert into TableSequence (tableName, nextFreeNumber) values ('TEST_NUMBER',1);
 insert into TableSequence (tableName, nextFreeNumber) values ('ENDRINGSNUMMER',1);
