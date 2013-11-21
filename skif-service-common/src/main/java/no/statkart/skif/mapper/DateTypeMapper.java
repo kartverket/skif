6c929e9b3d6ce2e041fa3124da4d8ec953d9d560
Leif Eirik Lislegård <lislei@statkart.no>
2013-11-21 12:38:52 +0100

    benytter fqn for java.util.Data
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 52183]

--- skif-service-common/src/main/java/no/statkart/skif/mapper/DateTypeMapper.java
+++ skif-service-common/src/main/java/no/statkart/skif/mapper/DateTypeMapper.java
@@ -5,8 +5,6 @@ import no.statkart.skif.exception.ImplementationException;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -14,23 +12,23 @@ import java.util.GregorianCalendar;
  * @author Roar Ingebrigtsen
  * @since 2.0
  */
-public class DateTypeMapper extends AbstractTypeMapper<XMLGregorianCalendar, Date, Mapping> {
+public class DateTypeMapper extends AbstractTypeMapper<XMLGregorianCalendar, java.util.Date, Mapping> {
 
     public DateTypeMapper() {
-        super(XMLGregorianCalendar.class, Date.class, Mapping.class);
+        super(XMLGregorianCalendar.class, java.util.Date.class, Mapping.class);
     }
 
     // Gir en kalender som er gregoriansk hele veien, uten noe skifte til juliansk
-    private GregorianCalendar createPureGregorianCalendar(Date date) {
+    private GregorianCalendar createPureGregorianCalendar(java.util.Date date) {
         GregorianCalendar calendar = new GregorianCalendar();
         calendar.clear();
         calendar.setTime(date);
-        calendar.setGregorianChange(new Date(Long.MIN_VALUE));
+        calendar.setGregorianChange(new java.util.Date(Long.MIN_VALUE));
         return calendar;
     }
 
     @Override
-    public XMLGregorianCalendar mapDomainObject(Date source) {
+    public XMLGregorianCalendar mapDomainObject(java.util.Date source) {
         final GregorianCalendar gregorianCalendar = createPureGregorianCalendar(source);
         try {
             return DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
@@ -40,7 +38,7 @@ public class DateTypeMapper extends AbstractTypeMapper<XMLGregorianCalendar, Dat
     }
 
     @Override
-    public Date mapWsapiObject(XMLGregorianCalendar source) {
+    public java.util.Date mapWsapiObject(XMLGregorianCalendar source) {
         GregorianCalendar instance = new GregorianCalendar();
         instance.clear();
         instance.set(source.getYear() > Integer.MIN_VALUE ? source.getYear() : 0, source.getMonth() > Integer.MIN_VALUE ? source.getMonth() - 1 : 0, source.getDay() > Integer.MIN_VALUE ? source.getDay() : 0, source.getHour() > Integer.MIN_VALUE ? source.getHour() : 0, source.getMinute() > Integer.MIN_VALUE ? source.getMinute() : 0, source.getSecond() > Integer.MIN_VALUE ? source.getSecond() : 0);
