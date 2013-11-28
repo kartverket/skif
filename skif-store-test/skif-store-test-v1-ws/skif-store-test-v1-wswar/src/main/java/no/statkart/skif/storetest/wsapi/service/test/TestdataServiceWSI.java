7f806b50cc3676ac76d26f0eea436f48f215fb5b
Henrik Fredholm <frehen@statkart.no>
2013-11-28 21:46:45 +0100

    SKIF-402: Redesign av Endringslogg. Tester kjører i SingleVm og JEE. Byttet ut mapping av SnapshotVersion til XMLGregorianCalendar med mapping til SnapshotVersion.
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 52503]

--- skif-store-test/skif-store-test-v1-ws/skif-store-test-v1-wswar/src/main/java/no/statkart/skif/storetest/wsapi/service/test/TestdataServiceWSI.java
+++ skif-store-test/skif-store-test-v1-ws/skif-store-test-v1-wswar/src/main/java/no/statkart/skif/storetest/wsapi/service/test/TestdataServiceWSI.java
@@ -2,6 +2,7 @@ package no.statkart.skif.storetest.wsapi.service.test;
 
 import no.statkart.skif.service.ws.ServiceWSI;
 import no.statkart.skif.storetest.wsapi.domain.*;
+import no.statkart.skif.storetest.wsapi.domain.basetyper.SnapshotVersion;
 import no.statkart.skif.storetest.wsapi.exception.ServiceException;
 
 import javax.jws.WebParam;
@@ -23,7 +24,7 @@ public interface TestdataServiceWSI extends ServiceWSI {
     public void saveAll(@WebParam(name = "snapshotTransfers") MockupSnapshotMap snapshotTransfers, @WebParam(name = "context") StoreTestContext context) throws ServiceException;
 
     /** @since 2.1 */
-    public void saveSnapshotTransfer(@WebParam(name = "snapshotVersion") XMLGregorianCalendar snapshotVersion, @WebParam(name = "mockupTransfer") MockupTransfer mockupTransfer, @WebParam(name = "context") StoreTestContext context) throws ServiceException;
+    public void saveSnapshotTransfer(@WebParam(name = "snapshotVersion") SnapshotVersion snapshotVersion, @WebParam(name = "mockupTransfer") MockupTransfer mockupTransfer, @WebParam(name = "context") StoreTestContext context) throws ServiceException;
 
     public void deleteObject(@WebParam(name = "id") long id, @WebParam(name = "tableName") String tableName, @WebParam(name = "context") StoreTestContext context) throws ServiceException;
 
