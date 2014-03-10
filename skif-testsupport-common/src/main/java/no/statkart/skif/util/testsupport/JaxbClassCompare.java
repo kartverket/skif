4498fc5269004a574d5e91ffd519db23fcd28768
Tor Egil Strand <strant@statkart.no>
2014-03-10 11:18:26 +0100

    Fiks for Java 7
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 55291]

--- skif-testsupport-common/src/main/java/no/statkart/skif/util/testsupport/JaxbClassCompare.java
+++ skif-testsupport-common/src/main/java/no/statkart/skif/util/testsupport/JaxbClassCompare.java
@@ -200,9 +200,9 @@ public class JaxbClassCompare {
         }
     }
 
-    private static class ClassComparator implements Comparator<Class<?>> {
+    private static class ClassComparator implements Comparator<Class> {
         @Override
-        public int compare(Class<?> o1, Class<?> o2) {
+        public int compare(Class o1, Class o2) {
             return o1.getName().compareTo(o2.getName());
         }
     }
