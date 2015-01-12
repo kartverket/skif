1519b5081d039f5299f5f028378888203cd6ca88
Tor Egil Strand <strant@statkart.no>
2015-01-12 15:05:44 +0100

    Sjekker inn en web service for å teste typer
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 62139]

---
+++ skif-service-test/skif-service-test-v1-ws/skif-service-test-v1-wswar/src/main/java/no/statkart/skif/skiftest/wsapi/service/types/TypeServiceWSBean.java
@@ -0,0 +1,22 @@
+package no.statkart.skif.skiftest.wsapi.service.types;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+/**
+ * Denne tjenesten er kun her for å teste ut hvordan andre rammeverke takler forskjellige typer. Den benytter ikke SKIF
+ * på noen måte.
+ */
+@WebService(
+        name = "TypeService",
+        serviceName = "TypeServiceWS",
+        targetNamespace = "http://skif.statkart.no/skiftest/wsapi/service/types")
+public class TypeServiceWSBean {
+    @WebMethod
+    @WebResult(targetNamespace = "http://skif.statkart.no/skiftest/wsapi/service/types")
+    public byte[] rawBytes(@WebParam(name = "bytes", targetNamespace = "http://skif.statkart.no/skiftest/wsapi/service/types") byte[] bytes) {
+        return bytes;
+    }
+}
