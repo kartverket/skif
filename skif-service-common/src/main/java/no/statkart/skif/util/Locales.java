cbe92aec0e8fe98377d74c2a16881d3d3ed9cd8c
Henrik Fredholm <frehen@statkart.no>
2015-05-21 06:54:47 +0100

    Lagt til Locales klasse
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 64192]

---
+++ skif-service-common/src/main/java/no/statkart/skif/util/Locales.java
@@ -0,0 +1,10 @@
+package no.statkart.skif.util;
+
+import java.util.Locale;
+
+/**
+ */
+public class Locales {
+    final public static Locale Bokmaal = new Locale("nb", "NO");
+    final public static Locale Nynorsk = new Locale("nn", "NO");
+}
