f92d73019446481b62422ab052d91666fe9cdd70
Tor Egil Strand <strant@statkart.no>
2015-08-19 14:15:15 +0100

    SKIF-523: En generisk hjelpeklasse for å kalle EJB-er fra andre deler av JEE enn JAX-WS
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 65471]

---
+++ skif-service-server/src/main/java/no/statkart/skif/service/util/ServiceRequestScopeTemplate.java
@@ -0,0 +1,76 @@
+package no.statkart.skif.service.util;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import no.statkart.skif.service.ServiceRequestContext;
+import no.statkart.skif.service.TxMode;
+import no.statkart.skif.service.annotation.CallId;
+import no.statkart.skif.service.scope.ServiceRequestScope;
+
+import java.security.Principal;
+import java.util.concurrent.Callable;
+
+/**
+ * Utfører gitt kode i et service request scope. På den måten kan man kalle inn til en SKIF-EJB fra vanlig JEE-kode.
+ * Denne klassen bør ikke brukes dersom man ikke vet at man befinner seg utenfor SKIF. Den er heller ikke laget for
+ * single-vm.
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class ServiceRequestScopeTemplate {
+    private final Provider<Long> callIdProvider;
+    private final ServiceRequestScope serviceRequestScope;
+
+    @Inject
+    public ServiceRequestScopeTemplate(@CallId Provider<Long> callIdProvider, ServiceRequestScope serviceRequestScope) {
+        this.callIdProvider = callIdProvider;
+        this.serviceRequestScope = serviceRequestScope;
+    }
+
+    /**
+     * Tilsvarende som {@link #execute(String, Principal, Runnable)}, men bruker klassenavnet til
+     * {@code Runnable}-implementasjonen som {@code serviceName}.
+     */
+    public void execute(Principal principal, Runnable runnable) {
+        execute(runnable.getClass().getName(), principal, runnable);
+    }
+
+    public void execute(String serviceName, Principal principal, Runnable runnable) {
+        ServiceRequestContext serviceRequestContext = createServiceRequestContext(serviceName, principal);
+
+        serviceRequestScope.enter();
+        try {
+            serviceRequestScope.seed(ServiceRequestContext.class, serviceRequestContext);
+            runnable.run();
+        } finally {
+            serviceRequestScope.exit();
+        }
+    }
+
+    /**
+     * Tilsvarende som {@link #execute(String, java.security.Principal, Callable)}, men bruker klassenavnet til
+     * {@code Callable}-implementasjonen som {@code serviceName}.
+     */
+    public <V> V execute(Principal principal, Callable<V> callable) throws Exception {
+        return execute(callable.getClass().getName(), principal, callable);
+    }
+
+    public <V> V execute(String serviceName, Principal principal, Callable<V> callable) throws Exception {
+        ServiceRequestContext serviceRequestContext = createServiceRequestContext(serviceName, principal);
+
+        serviceRequestScope.enter();
+        try {
+            serviceRequestScope.seed(ServiceRequestContext.class, serviceRequestContext);
+            return callable.call();
+        } finally {
+            serviceRequestScope.exit();
+        }
+    }
+
+    private ServiceRequestContext createServiceRequestContext(String serviceName, Principal principal) {
+        ServiceRequestContext serviceRequestContext = new ServiceRequestContext(TxMode.NOT_IN_EJB, false, null);
+        serviceRequestContext.setCallerPrincipal(principal);
+        serviceRequestContext.setServicename(serviceName);
+        serviceRequestContext.setCallId(callIdProvider.get());
+        return serviceRequestContext;
+    }
+}
