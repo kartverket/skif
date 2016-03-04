9cf258cc26991eaa41ed1eb8e6fa0ee05534b5f7
Tor Egil Strand <strant@statkart.no>
2016-03-04 13:38:25 +0100

    SKIF-567: Mapping av Timestamp til OJDBC som ikke blir forvirrent ved overgang for sommertid til vintertid
    [git-p4: depot-paths = "//skif/SkifKode/trunk/": change = 70270]

---
+++ skif-store-server/src/main/java/no/statkart/skif/persistence/hibernate/type/OracleLocalTimestamp.java
@@ -0,0 +1,79 @@
+package no.statkart.skif.persistence.hibernate.type;
+
+import oracle.jdbc.OraclePreparedStatement;
+import oracle.sql.TIMESTAMPLTZ;
+import org.hibernate.HibernateException;
+import org.hibernate.usertype.UserType;
+
+import java.io.Serializable;
+import java.sql.*;
+import java.util.Objects;
+
+/**
+ * Hvis man bruker {@link java.sql.PreparedStatement#setTimestamp(int, java.sql.Timestamp)}, så klarer ikke
+ * JDBC/databasen å skille mellom den doble timen når vi går over fra sommertid til vintertid, selv om datatypen i
+ * databasen er {@code TIMESTAMP WITH LOCAL TIME ZONE}. Må gjøre oversettelsen før vi oversender dataene til JDBC.
+ * Dersom datatypen i databasen ikke er {@code TIMESTAMP WITH LOCAL TIME ZONE}, så er det meningsløst å bruke denne klasen.
+ */
+public class OracleLocalTimestamp implements UserType {
+
+    private static final int[] SQL_TYPES = new int[]{Types.TIMESTAMP};
+
+    @Override
+    public int[] sqlTypes() {
+        // Er oracle.jdbc.OracleTypes.TIMESTAMPLTZ er alternativ her?
+        return SQL_TYPES;
+    }
+
+    @Override
+    public Class returnedClass() {
+        return Timestamp.class;
+    }
+
+    @Override
+    public boolean equals(Object x, Object y) throws HibernateException {
+        return Objects.equals(x, y);
+    }
+
+    @Override
+    public int hashCode(Object x) throws HibernateException {
+        return x.hashCode();
+    }
+
+    @Override
+    public Object nullSafeGet(ResultSet rs, String[] names, Object owner) throws HibernateException, SQLException {
+        return rs.getTimestamp(names[0]);
+    }
+
+    @Override
+    public void nullSafeSet(PreparedStatement st, Object value, int index) throws HibernateException, SQLException {
+        OraclePreparedStatement ops = st.unwrap(OraclePreparedStatement.class);
+        ops.setTIMESTAMPLTZ(index, new TIMESTAMPLTZ(ops.getConnection(), (Timestamp) value));
+    }
+
+    @Override
+    public Object deepCopy(Object value) throws HibernateException {
+        return value;
+    }
+
+    @Override
+    public boolean isMutable() {
+        return false;
+    }
+
+    @Override
+    public Serializable disassemble(Object value) throws HibernateException {
+        return (Timestamp) value;
+    }
+
+    @Override
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        //noinspection RedundantCast
+        return (Timestamp) cached;
+    }
+
+    @Override
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return original;
+    }
+}
