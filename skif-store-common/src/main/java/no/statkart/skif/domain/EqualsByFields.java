55ca54df2d7215169e9247e456205cd115e4c6fc
Tor Egil Strand <strant@kartverket.no>
2017-04-24 13:28:07 +0200

    SKIF-633: Oppdag sirkulære referanser og unngå uendelig rekursjon

--- skif-store-common/src/main/java/no/statkart/skif/domain/EqualsByFields.java
+++ skif-store-common/src/main/java/no/statkart/skif/domain/EqualsByFields.java
@@ -8,6 +8,9 @@ import java.util.*;
 public class EqualsByFields {
     private final List<HandlerEntry> handlers = new ArrayList<>();
 
+    // For å unngå rekursivitet
+    private final Set<Pair> processed = new HashSet<>();
+
     public EqualsByFields() {
         addHandler(List.class, new ListHandler());
         addHandler(Set.class, new SetHandler());
@@ -21,6 +24,12 @@ public class EqualsByFields {
     }
 
     public boolean isEqualByFields(@Nullable Object o1, @Nullable Object o2) {
+        if (!processed.add(new Pair(o1, o2))) {
+            // Det kan hende koden som la paret inn ikke har kjørt sammenligningen enda, men den kommer til å gjøre det,
+            // med mindre noe annet feiler først.
+            return true;
+        }
+
         if (o1 instanceof EqualityByFields) {
             EqualityByFields ebf1 = (EqualityByFields) o1;
             return ebf1.equalsByFields(o2, this);
@@ -59,6 +68,35 @@ public class EqualsByFields {
 
         public EqualityHandler getHandler() {
             return handler;
+        }
+    }
+
+    /**
+     * Klasse som representerer et par med objekter. Likhet er på identiten til de to objektene.
+     */
+    private static class Pair {
+        @Nullable private final Object o1;
+        @Nullable private final Object o2;
+
+        private Pair(@Nullable Object o1, @Nullable Object o2) {
+            this.o1 = o1;
+            this.o2 = o2;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair pair = (Pair) o;
+
+            // Det er med vilje at denne sjekker på identitet
+            return o1 == pair.o1 && o2 == pair.o2;
+        }
+
+        @Override
+        public int hashCode() {
+            return System.identityHashCode(o1) + System.identityHashCode(o2);
         }
     }
 }
