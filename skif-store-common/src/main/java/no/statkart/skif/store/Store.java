f65d3cc1a9fff98114fecca107db8d2dc2a92060
Tor Egil Strand <strant@kartverket.no>
2017-07-17 12:08:37 +0200

    Fikser Javadoc

--- skif-store-common/src/main/java/no/statkart/skif/store/Store.java
+++ skif-store-common/src/main/java/no/statkart/skif/store/Store.java
@@ -425,7 +425,7 @@ public interface Store {
      * kan metoden også kalles uten en aktiv unit-of-work.
      *
      * @return transfer med endrede objekter
-     * @see {@link WrappableStoreSession}
+     * @see WrappableStoreSession#getSnapshot()
      */
     UnitOfWorkTransfer getSnapshot();
 
@@ -435,7 +435,7 @@ public interface Store {
      * kalles hvis en unit-of-work er aktiv. På serveren kan metoden også kalles uten en aktiv unit-of-work.
      *
      * @return transfer med endrede objekter
-     * @see {@link WrappableStoreSession}
+     * @see WrappableStoreSession#getSessionSnapshot()
      */
     UnitOfWorkTransfer getSessionSnapshot();
 
