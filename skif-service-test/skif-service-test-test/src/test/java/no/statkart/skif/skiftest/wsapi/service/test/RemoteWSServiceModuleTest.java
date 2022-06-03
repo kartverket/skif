b0831d5e3b7a59464489de2de929b94a096a0fd4
Alexander Solstad Larsen <Alexander.Solstad.Larsen@kartverket.no>
2022-06-03 09:28:55 +0200

    SKIF-749 Fikset feil oppsett av WSService-basert test

--- skif-service-test/skif-service-test-test/src/test/java/no/statkart/skif/skiftest/wsapi/service/test/RemoteWSServiceModuleTest.java
+++ skif-service-test/skif-service-test-test/src/test/java/no/statkart/skif/skiftest/wsapi/service/test/RemoteWSServiceModuleTest.java
@@ -1,12 +1,16 @@
 package no.statkart.skif.skiftest.wsapi.service.test;
 
+import no.statkart.skif.SkifModule;
 import no.statkart.skif.config.Configuration;
 import no.statkart.skif.module.ModuleConfiguration;
+import no.statkart.skif.module.ModuleStrategyFactory;
+import no.statkart.skif.service.module.client.ClientModuleStrategyFactory;
+import no.statkart.skif.service.module.common.RemoteServerModule;
 import no.statkart.skif.service.module.common.RemoteWSServiceModule;
 import no.statkart.skif.service.module.server.WSServerServiceModule;
 import no.statkart.skif.skiftest.config.SkifTestGroup1Services;
 import no.statkart.skif.skiftest.config.SkifTestServerModule;
-import no.statkart.skif.skiftest.service.test.SkifTestClientModule;
+import no.statkart.skif.skiftest.service.SkifTestServiceContext;
 import no.statkart.skif.skiftest.wsapi.SkifTestServiceContextMapper;
 import no.statkart.skif.skiftest.wsapi.domain.SkifTestContext;
 import no.statkart.skif.skiftest.wsapi.exception.ServiceException;
@@ -33,6 +37,8 @@ public class RemoteWSServiceModuleTest extends SkifTestCase {
         setSingleVmServerModuleClass(ServerModule.class);
     }
 
+
+
     @Test
     public void testTest1ServiceWSDirect() throws ServiceException {
         Test1Service test1Service = injector.getInstance(Test1Service.class);
@@ -45,7 +51,12 @@ public class RemoteWSServiceModuleTest extends SkifTestCase {
         Assert.assertEquals("Hello1: TestWS", result);
     }
 
-    public static class ClientModule extends SkifTestClientModule {
+    public static class ClientModule extends SkifModule {
+
+        @Override
+        protected ModuleStrategyFactory defineDefaultModuleStrategyFactory() {
+            return new ClientModuleStrategyFactory();
+        }
 
         public ClientModule(ModuleConfiguration moduleConfiguration) {
             super(moduleConfiguration);
@@ -53,9 +64,9 @@ public class RemoteWSServiceModuleTest extends SkifTestCase {
 
         @Override
         protected void configure() {
-            super.configure();
+            install(new RemoteServerModule(moduleConfiguration).setServiceContextClass(SkifTestServiceContext.class));
 
-            List<Class<?>> wsServices = new ArrayList<Class<?>>(1);
+            List<Class<?>> wsServices = new ArrayList<>(1);
             wsServices.add(Test1Service.class);
 
             install(new RemoteWSServiceModule(moduleConfiguration, wsServices));
