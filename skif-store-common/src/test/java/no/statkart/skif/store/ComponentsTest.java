85b6f88c9db81636c6ace5ccde3a3492587b46bc
Henning Håkonsen <henninghaakonsen@gmail.com>
2025-10-08 12:58:37 +0200

    Legger til test som verifiserer at vi fjerner eksisterende elementer fra lista når vi sender inn null

--- skif-store-common/src/test/java/no/statkart/skif/store/ComponentsTest.java
+++ skif-store-common/src/test/java/no/statkart/skif/store/ComponentsTest.java
@@ -61,6 +61,13 @@ public class ComponentsTest {
         Assertions.assertThat(set).isEmpty();
     }
 
+    @Test
+    public void testSetFrom_nullToEmptyFromNotEmpty() {
+        HashSet<TestComponent> set = Sets.newHashSet(new TestComponent(), new TestComponent());
+        Components.setFrom(set, null);
+        Assertions.assertThat(set).isEmpty();
+    }
+
     private static class TestComponent implements Component {
         private static final long serialVersionUID = 1L;
     }
