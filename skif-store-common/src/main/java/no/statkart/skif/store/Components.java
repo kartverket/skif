3f0fb7303e094982998ed08cdf2ef80c5fbaef2e
Henning Håkonsen <henninghaakonsen@gmail.com>
2025-10-09 13:44:51 +0200

    Revert "[TH-695] Bedre håndtering av null som parameter til setFrom (#118)" (#119)
    This reverts commit cbc779a807ab41d74b3160ef740b1d2c77682f02.

--- skif-store-common/src/main/java/no/statkart/skif/store/Components.java
+++ skif-store-common/src/main/java/no/statkart/skif/store/Components.java
@@ -150,11 +150,6 @@ public class Components {
     }
 
     static public <E extends Component> void setFrom(Collection<E> collection, Collection<E> newElements) {
-        if (newElements == null){
-            collection.clear();
-            return;
-        }
-
         // Må ta en kopi av newElements, i tilfelle newElements er den samme som collection eller en form for wrapper for den.
         ImmutableList<E> copy = ImmutableList.copyOf(newElements);
         collection.clear();
