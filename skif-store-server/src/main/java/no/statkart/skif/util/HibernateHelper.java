f7851c37408075c12f057aea55de7776fdc778f0
Leif Lislegård <lislei@kartverket.no>
2025-11-14 13:32:18 +0100

    TH-2387 QA Rydder ubrukte metoder i HibernateHelper

--- skif-store-server/src/main/java/no/statkart/skif/util/HibernateHelper.java
+++ skif-store-server/src/main/java/no/statkart/skif/util/HibernateHelper.java
@@ -1,53 +1,16 @@
 package no.statkart.skif.util;
 
-import no.statkart.skif.store.persistence.SessionSelector;
 import org.hibernate.Session;
 import org.hibernate.metadata.ClassMetadata;
 import org.hibernate.persister.entity.AbstractEntityPersister;
 import org.hibernate.persister.entity.Queryable;
-import org.hibernate.proxy.HibernateProxy;
 
-import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.Collections;
 
 /**
  * @author Henrik Fredholm
  */
 public class HibernateHelper {
-    /**
-     * @deprecated bruk heller try-with-resource
-     */
-    @Deprecated
-    public static void close(Statement statement) {
-        JDBCHelper.close(statement);
-    }
-
-    /**
-     * @deprecated bruk heller try-with-resource
-     */
-    @Deprecated
-    public static void close(PreparedStatement preparedStatement, SessionSelector sessionSelector) {
-        try {
-            close(preparedStatement);
-        } finally {
-            if (sessionSelector != null) sessionSelector.close();
-        }
-    }
-
-    /**
-     * Finner {@code ClassMetadata} for et gitt objekt. Denne er å foretrekke fremfor {@code {@link #getClassMetadata(Session, Class)}}
-     * og å gjøre tilsvarende manuelt, da den inneholder logikk for å håndtere proxy-objekter.
-     */
-    public static ClassMetadata getClassMetadata(Session session, Object o) {
-        Class c;
-        if (o instanceof HibernateProxy) {
-            c = ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass();
-        } else {
-            c = o.getClass();
-        }
-        return getClassMetadata(session, c);
-    }
 
     public static ClassMetadata getClassMetadata(Session session, Class c) {
         return session.getSessionFactory().getClassMetadata(c);
