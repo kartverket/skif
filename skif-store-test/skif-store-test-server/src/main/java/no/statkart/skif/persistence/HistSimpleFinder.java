b8e9a51fb62bdf4a8ef87984653e22a28e544f47
Martin Halleland <martin.halleland@gmail.com>
2026-01-19 09:56:02 +0100

    TH-2324: Matcher navn i query med klasse

--- skif-store-test/skif-store-test-server/src/main/java/no/statkart/skif/persistence/HistSimpleFinder.java
+++ skif-store-test/skif-store-test-server/src/main/java/no/statkart/skif/persistence/HistSimpleFinder.java
@@ -53,9 +53,9 @@ public class HistSimpleFinder {
     public Set<HistSimpleId<?>> findHistSimpleIdsForTextUsingHibernate(String text, int testsetNummer, SnapshotVersion snapshotVersion) {
         try (SessionSelector sessionSelector = sessionSelectorProvider.get()) {
             List<HistSimple> histSimples = sessionSelector.get(snapshotVersion)
-                .createQuery("from HistSimple where text = :text and testsetNumber = :testsetNumber", HistSimple.class)
+                .createQuery("from HistSimple where text = :text and testSetNumber = :testSetNumber", HistSimple.class)
                 .setParameter("text", text)
-                .setParameter("testsetNumber", testsetNummer)
+                .setParameter("testSetNumber", testsetNummer)
                 .list();
             Set<HistSimpleId<?>> histSimpleIds = new HashSet<>(histSimples.size());
             for (HistSimple histSimple : histSimples) {
