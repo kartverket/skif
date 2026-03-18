fbba03fe030f3fb4549c40202dc0983c7387f8bc
Stian Alexander Solli <stian.alexander.solli@kartverket.no>
2026-03-18 14:06:07 +0100

    TH-2535 tester fix for path naming for linux

--- database/src/main/sql/oracle/tablespace/CreateTablespace.sql
+++ database/src/main/sql/oracle/tablespace/CreateTablespace.sql
@@ -8,6 +8,6 @@
 
 --Create tablespace
 CREATE TABLESPACE "@db_tablespace@"
-DATAFILE '@db_dataroot_dir@\@db_tablespace@.DBF' SIZE 100M
+DATAFILE '@db_dataroot_dir@/@db_tablespace@.DBF' SIZE 100M
 LOGGING
 EXTENT MANAGEMENT LOCAL SEGMENT SPACE MANAGEMENT AUTO;
